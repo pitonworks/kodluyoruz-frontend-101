a146583190c7fc4d77582d6a243154b034e840ac
pitonworks <omergungor99@gmail.com>
2024-08-10 13:29:31 +0300

    010

--- 04-javascript-kategorisi/js/010-dom-icerisinden-oge-secimi.js
+++ 04-javascript-kategorisi/js/010-dom-icerisinden-oge-secimi.js
@@ -5,10 +5,10 @@
 
 // let h2 = document.getElementsByTagName('h2')
 let title = document.getElementById('title')
-title.innerHTML = "Degisen Bilgi"
+title.innerHTML = "Değişen bilgi"
 console.log(title.innerHTML)
 
 let link = document.querySelector("#kodluyoruzLink")
-link.innerHTML += " degisti"
+link.innerHTML += " değişti"
 link.style.color = "red"
-link.classList.add('btn')
+link.classList.add("btn")
