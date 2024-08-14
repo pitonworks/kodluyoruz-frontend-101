faac32d809c22bc717417c5042a1116613e2b195
pitonworks <omergungor99@gmail.com>
2024-08-14 11:53:12 +0300

    name1

--- 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
+++ 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
@@ -6,3 +6,5 @@ let fullName = prompt("Lutfen Adinizi Giriniz: ")
 let greeting = document.querySelector("#greeting")
 
 greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
+
+let = name1 = prompt("Lütfen Adınızı Girin: ")
