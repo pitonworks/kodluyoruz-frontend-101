f0811bb9e1444226d5554e57132f5d628c46a93f
pitonworks <omergungor99@gmail.com>
2024-08-19 14:19:23 +0300

    childs

--- 04-javascript-kategorisi/js/012-liste-icindeki-ogeye-ulasmak-veya-yeni-oge-eklemek.js
+++ 04-javascript-kategorisi/js/012-liste-icindeki-ogeye-ulasmak-veya-yeni-oge-eklemek.js
@@ -1,9 +1,9 @@
 // ********** liste icerisindeki son elemana ulasmak veya eleman eklemek **********
 
-let lastChild = document.querySelector("ul#list>li:last-child")
+let lastChild = document.querySelector("ul#list>li:last-child") // son öğe değiştirdi "last-child"
 lastChild.innerHTML = "son oge degisti..."
 
-let firstChild = document.querySelector("ul#list>li:first-child")
+let firstChild = document.querySelector("ul#list>li:first-child") // ilk öğe değişti "first-child"
 firstChild.innerHTML = "ilk oge degisti..."
 
 let ulDOM = document.querySelector("ul#list")
@@ -17,4 +17,4 @@ ulDOM.prepend(liDOM)
 let lastChild = document.querySelector("ul#list>li:last-child")
 lastChild.innerHTML = "son oge degisti..."
 
-// commit check bab
+
