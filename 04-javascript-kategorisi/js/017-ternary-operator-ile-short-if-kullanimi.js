9371d547ba4285c30ec2bf09b803b8ef73b35aeb
pitonworks <omergungor99@gmail.com>
2024-08-20 12:58:01 +0300

    ternary2

--- 04-javascript-kategorisi/js/017-ternary-operator-ile-short-if-kullanimi.js
+++ 04-javascript-kategorisi/js/017-ternary-operator-ile-short-if-kullanimi.js
@@ -13,3 +13,11 @@ let info = document.querySelector("#info")
 info.innerHTML = `${userName ? userName : "Kullani Bilginiz Bulunamadi :("}`
 
 // ternary
+
+var money = 40;
+var canBuy = 
+    (money < 17) ? "Satın alamazsın..":
+    (money > 30) ? "Satın alabilirsin..":
+    "Para miktarını girmen gerekmektedir..";
+
+console.log(canBuy) // "Satın alabilirsin.."
