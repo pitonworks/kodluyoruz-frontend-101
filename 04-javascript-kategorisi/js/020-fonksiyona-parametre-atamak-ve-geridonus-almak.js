5fba9e8411c2e25ba0d6195fbd66e06f238c5f20
pitonworks <omergungor99@gmail.com>
2024-08-22 21:29:59 +0300

    yarıçapa göre alan hesabı fonksiyonu

--- 04-javascript-kategorisi/js/020-fonksiyona-parametre-atamak-ve-geridonus-almak.js
+++ 04-javascript-kategorisi/js/020-fonksiyona-parametre-atamak-ve-geridonus-almak.js
@@ -42,3 +42,13 @@ function fonksiyonunAdi (parametre1, parametre2,...parametreN) {
     // ALINAN PARAMETRELER İLE YAPILMAK İSTENEN İŞLEMLER
     return fonksiyonunCiktisi
 }
+//********************************************************************
+
+const PI = 3.14; // Formülde kullandığımız sabit sayı pi'yi bu şekilde alabiliriz.
+function daireAlaniHesaplama(r) {// Fonksiyonumuz, r parametresini alıyor. {
+    var islemSonucu = PI*r*r; // Dairenin alanını hesaplayacak işlemimiz.
+    return islemSonucu;		 // return ifadesiyle sonucu dönüyoruz.
+}
+
+var donenSonuc = daireAlaniHesaplama(3); // Argüman olarak, alanını hesaplamak istediğimiz herhangi bir dairenin yarıçapı için 3 rakamı verildi. 
+console.log(donenSonuc); // 28.25999999 sonucunu göreceğiz.
