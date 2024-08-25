d491cb963faf90e92a655a3a37d7156ac164d06f
pitonworks <omergungor99@gmail.com>
2024-08-25 16:36:21 +0300

    dom

--- 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -9,5 +9,4 @@ function domClick() {
     console.log("etkinlik denetlendi")
     this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
 }
-//com
-//com2
+// dom etkinlikleri
