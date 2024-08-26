85ce0730fcd5825baad2d4f752063da2026cce64
pitonworks <omergungor99@gmail.com>
2024-08-26 22:30:35 +0300

    form event

--- 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -59,3 +59,30 @@ function cuttingFunction(event){
   
 //console ekranı : etkinlik tipi: cut
 
+// form event
+<form action="#" id="selectColor">
+  <label for="color">Choose a color</label>
+    <select name="colors" id="select">
+      <option value="red">RED</option>
+      <option value="blue">BLUE</option>
+      <option value="purple">PURPLE</option>
+      <option value="orange">ORANGE</option>
+      <option value="pink">PINK</option>
+    </select>
+    <br><br>
+  <input type="submit" value="Submit">
+</form>   
+      
+//id=select olan nesne çağrılarak color değişkenine atandı
+const color = document.querySelector("#select");
+//color değişkenine change etkinliği etkinliği atandı ve fonksiyon ismi parametre olarak atandı
+color.addEventListener('change' ,selectBox);
+
+//selectBox isimli fonksiyon ile selectbox her seçildiğinde etkinlik tipi =change ve değeri consola yazılmaktadır 
+function selectBox(event){
+  console.log("Etkinlik tipi : " + event.type);
+  console.log("deger : " + event.target.value);
+  }
+  
+// Selectbox içinde "blue" değerini seçtiğimiz takdirde;
+// ekran çıktısı: Etkinlik tipi : change, deger : blue﻿
