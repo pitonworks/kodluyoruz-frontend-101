7d084961e4be48c834ad1a479be05bb334cadddc
pitonworks <omergungor99@gmail.com>
2024-08-26 22:30:01 +0300

    mouse click

--- 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -37,3 +37,13 @@ button.addEventListener('click',btnClicked);
 
 // Çıktı : Buton Tıklandı!!!
 
+// mouse click
+//id=text olan buton çağrıldı
+//dblclick=çift tıklama etkinliği nesneye atandı,fonksiyon ismi parametre olarak tanımlandı
+const button = document.querySelector("#text");
+button.addEventListener('dblclick',clickFonksiyonu);
+  
+//fonksiyonda id=text olan element çağrılarak innerHTML ile içeriği değiştirildi.
+function clickFonksiyonu(){
+  document.getElementById("text").innerHTML = "NEW FORM";
+  }
