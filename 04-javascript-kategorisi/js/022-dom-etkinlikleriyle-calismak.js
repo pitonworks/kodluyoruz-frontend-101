4d0e8e4355ada36ab5c7c3f30390e16d9cb14f67
pitonworks <omergungor99@gmail.com>
2024-08-26 22:28:31 +0300

    metot1

--- 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -9,4 +9,15 @@ function domClick() {
     console.log("etkinlik denetlendi")
     this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
 }
-// dom etkinlikleri 1
+// metot 1
+element.addEventListener(event, function);
+// id =button olan nesne seçildi
+const buton = document.querySelector("id");
+// Nesneye bir etkinlik atandı..
+const buton = document.querySelector("#button");
+//Burada ilk olarak tip ('click') ,ikinci olarak da fonksiyonun kendisi parametre olarak eklenir
+buton.addEventListener('click' , function(){
+  alert("Buton Tıklandı");
+       })
+ 
+// Çıktı : Buton Tıklandı
