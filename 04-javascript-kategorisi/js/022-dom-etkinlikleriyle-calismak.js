4f7c7bb1f8d7d8e22440c4974752e104bbae58c4
pitonworks <omergungor99@gmail.com>
2024-08-26 22:30:18 +0300

    keyboard event

--- 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -47,3 +47,15 @@ button.addEventListener('dblclick',clickFonksiyonu);
 function clickFonksiyonu(){
   document.getElementById("text").innerHTML = "NEW FORM";
   }
+
+//id=fname olan nesne çağrılarak keyboard değişkenine atandı
+const keyboard =document.querySelector('#fname');
+//keyboard isimli değişkene "cut" etkinliği atandı ve fonksiyon ismi parametre olarak atandı
+keyboard.addEventListener("cut",cuttingFunction);
+//cuttingFunction isimli fonksiyon ile form inputuna yazılan girdileri klavye ctrl+c ile kesildiğinde etkinlik çalışmaktadır
+function cuttingFunction(event){
+  console.log("etkinlik tipi: " , event.type)
+  } 
+  
+//console ekranı : etkinlik tipi: cut
+
