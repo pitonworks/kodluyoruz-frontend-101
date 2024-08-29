56d0a00615a6a399fccc196df1c45637da7f34eb
pitonworks <omergungor99@gmail.com>
2024-08-29 22:13:36 +0300

    last comm

--- 04-javascript-kategorisi/js/025-localStorage-icerisinde-farkli-turde-bilgilerin-saklanmasi.js
+++ 04-javascript-kategorisi/js/025-localStorage-icerisinde-farkli-turde-bilgilerin-saklanmasi.js
@@ -3,7 +3,7 @@
 
 let user = {userName: "omergungor11", isActive: true}
 console.log(user)
-localStorage.setItem('userInfo', JSON.stringify(user) )
+localStorage.setItem('userInfo', JSON.stringify(user) ) // objeyi stringe dönüştürüp aldık
 
 
 let userInfo = localStorage.getItem('userInfo') // bilgiyi al
@@ -17,3 +17,9 @@ let items = [1,2,3, user]
 localStorage.setItem('newItems', JSON.stringify(items))
 
 let newItems = JSON.parse( localStorage.getItem('newItem') ) //JSON Parse ile localStorage icindeki bilginin cekilip kullanilmasi..
+
+let userStatus = {userName: 'kodluyoruz', isActive: true}
+localStorage.setItem('user', userStatus),
+
+let userStatus1 = {userName: 'kodluyoruz', isActive: true}
+localStorage.setItem('user', JSON.stringify(userStatus1))
